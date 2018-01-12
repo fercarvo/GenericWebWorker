8800a3f447a898b4451ff67b5d435e4e9a846270
Fernando Carvajal <efcu93@gmail.com>
2018-01-12 00:48:46 -0500

    webworker,js paso a ser un metodo de GenericWebWorker

--- genericWebWorker.js
+++ genericWebWorker.js
@@ -1,42 +1,43 @@
-/*
-    GenericWebWorker v1.01
-    
-    Author, Edgar Fernando Carvajal Ulloa <efcu93@gmail.com>, <efcarvaj@espol.edu.ec>
-    A Generic Javascript WebWorker where the user can execute and pass functions
-    
-    This WebWorker does not require a external WW file.
-
-    constructor:
-        data, information to be passed to the ww
-        context, the context where the functions are, if not mention, window
-        functions, Array of functions manes to be passed ej ["fn_1", "printerData"]
+/**
+* GenericWebWOrker v1.01
+* 
+* A Generic Javascript WebWorker that allows the user to execute and pass functions
+* Is Fileless, for what the user does not need a file.js to request
+*
+* @author, Edgar Fernando Carvajal Ulloa <efcu93@gmail.com>, <efcarvaj@espol.edu.ec>
+* {@link https://github.com/fercarvo/GenericWebWorker}
 */
 class GenericWebWorker {
+    /**
+    * @param    {any}    data       Any value to be passed to the WW, is the first parameter of the callback
+    * @param    {Array}  functions  Array of functions names presented in the context
+    * @param    {object} context    Context where the functions are, window default.
+    */
     constructor(data, functions, context = window) {
         this.data = data
         this.functions = []
         this.callback = null 
 
         if (context && Array.isArray(functions) && functions.length > 0) {
-            if (functions.some(fn => 'function' !== typeof context[fn] ))
+            if (functions.some(fn => 'function' !== typeof context[fn] )) {
                 throw new Error(`Some of these next (${functions}) are not functions`)
-
+            }
             this.functions = functions.map(fn => this.fn_string( context[fn] ))
         }
     }
 
     exec(cb) {
         return new Promise((resolve, reject) => {
-            if (typeof cb !== 'function')
+            if (typeof cb !== 'function'){
                 throw new Error(`(${cb}) is not a function`)
-
-            var params = cb.toString()
-            if (params.substring(params.indexOf('(')+1,params.indexOf(')')).split(',').length !== this.functions.length+1)
-                throw new Error("Invalid number of arguments, use ex. (data, fn1, fn2...)")
-
+            }
             this.callback = this.fn_string(cb)
 
-            var worker_link = window.URL.createObjectURL( new Blob(["onmessage = function (e) { try { var functions = {}; var args = []; e.data.functions.forEach(fn => { functions[fn.name] = new Function(fn.args, fn.body); args = [...args, fn.name];}); var callback = new Function( e.data.callback.args, e.data.callback.body); args = args.map(fn_name => functions[fn_name]); args = [e.data.data, ...args]; try { var result = callback.apply(this, args); postMessage(result); close(); } catch (e) { throw new Error(`FunctionError: ${e}`);}} catch (e) {postMessage({error: e.toString()}); close();} }"]))
+            if (this.callback.args.split(',').length !== this.functions.length + 1) {
+                throw new Error("Invalid number of arguments, use ex. (data, fn1, fn2...)")
+            }
+            var worker_string = this.fn_string(this.worker).body
+            var worker_link = window.URL.createObjectURL( new Blob([worker_string]) )
             var worker = new Worker(worker_link)
 
             worker.postMessage({ 
@@ -63,6 +64,45 @@ class GenericWebWorker {
                 worker = null, worker_link = null
             }
         })
+    }
+
+    //The WebWorker that makes the magic, 1st to string and then to Blob
+    worker() {
+        onmessage = function (e) {
+            try {
+                var functions = {} 
+                var args = []
+
+                e.data.functions.forEach(fn => {
+                    functions[fn.name] = new Function(fn.args, fn.body)
+                    args = [...args, fn.name] //["fn_1", "fn_2", "fn_3"]
+                })
+
+                var callback = new Function( e.data.callback.args, e.data.callback.body) //fn to execute
+
+                args = args.map(fn_name => functions[fn_name])
+                args = [e.data.data, ...args]
+
+                try {
+                    var result = callback.apply(this, args)
+
+                    if (!isPromise(result)) //If not a promise
+                        return postMessage(result)
+
+                    result.then(res => postMessage(res))
+                    result.catch(e => postMessage({error: e}))
+
+                } catch (e) {
+                    throw new Error(`FunctionError: ${e}`)
+                }
+            } catch (e) {
+                postMessage({error: e.message})
+            }
+        }
+
+        function isPromise(obj) { //Check if the return of a function is a promise
+            return !!obj && (typeof obj === 'object' && typeof obj.then === 'function')
+        }
     }
 
     fn_string(fn) {
--- webworker.js
+++ webworker.js
@@ -1,31 +1,36 @@
 //This file is not used, just for development purpose.
-//Used in line 39 of genericWebWorker.js
 onmessage = function (e) {
-	try {
-		var functions = {}; 
-		var args = [];
+    try {
+        var functions = {} 
+        var args = []
 
-		e.data.functions.forEach(fn => {
-			functions[fn.name] = new Function(fn.args, fn.body);
-			args = [...args, fn.name]; //["fn_1", "fn_2", "fn_3"]
-		});
+        e.data.functions.forEach(fn => {
+            functions[fn.name] = new Function(fn.args, fn.body)
+            args = [...args, fn.name] //["fn_1", "fn_2", "fn_3"]
+        })
 
-		var callback = new Function( e.data.callback.args, e.data.callback.body); //fn to execute
+        var callback = new Function( e.data.callback.args, e.data.callback.body) //fn to execute
 
-		args = args.map(fn_name => functions[fn_name]);
-		args = [e.data.data, ...args];
+        args = args.map(fn_name => functions[fn_name])
+        args = [e.data.data, ...args]
 
-		try {
-			var result = callback.apply(this, args);
-			postMessage( result ); close();
-		} catch (e) {
-			throw new Error(`FunctionError: ${e}`);
-		}
-	} catch (e) {
-		postMessage({error: e.toString()}); close();
-	}
+        try {
+            var result = callback.apply(this, args)
+
+            if (!isPromise(result)) //If not a promise
+                return postMessage(result)
+
+            result.then(res => postMessage(res))
+            result.catch(e => postMessage({error: e}))
+
+        } catch (e) {
+            throw new Error(`FunctionError: ${e}`)
+        }
+    } catch (e) {
+        postMessage({error: e.message})
+    }
 }
 
-//The WebWorker to be embebed in Blob
-//onmessage = function (e) { try { var functions = {}; var args = []; e.data.functions.forEach(fn => { functions[fn.name] = new Function(fn.args, fn.body); args = [...args, fn.name];}); var callback = new Function( e.data.callback.args, e.data.callback.body); args = args.map(fn_name => functions[fn_name]); args = [e.data.data, ...args]; try { var result = callback.apply(this, args); postMessage(result); close(); } catch (e) { throw new Error(`FunctionError: ${e}`);}} catch (e) {postMessage({error: e.toString()}); close();} }
-var string_ww = "onmessage = function (e) { try { var functions = {}; var args = []; e.data.functions.forEach(fn => { functions[fn.name] = new Function(fn.args, fn.body); args = [...args, fn.name];}); var callback = new Function( e.data.callback.args, e.data.callback.body); args = args.map(fn_name => functions[fn_name]); args = [e.data.data, ...args]; try { var result = callback.apply(this, args); postMessage(result); close(); } catch (e) { throw new Error(`FunctionError: ${e}`);}} catch (e) {postMessage({error: e.toString()}); close();} }"
+function isPromise(obj) { //Check if the return of a function is a promise
+    return !!obj && (typeof obj === 'object' && typeof obj.then === 'function')
+}
